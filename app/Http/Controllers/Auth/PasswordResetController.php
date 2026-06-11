8c984e196a773a6b31b8dc0f4bb3ecfb0ede099c
Manishraii-web <raimanish489@gmail.com>
2026-06-11 16:09:45 +0545

    complete controller and service for reset password

--- app/Http/Controllers/Auth/PasswordResetController.php
+++ app/Http/Controllers/Auth/PasswordResetController.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\ResetPassword\ForgotPasswordRequest;
+use App\Http\Requests\ResetPassword\ResetPasswordRequest;
 use App\Services\PasswordReset\PasswordResetService;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Password;
 
 class PasswordResetController extends Controller
 {
@@ -13,7 +16,25 @@ class PasswordResetController extends Controller
     return view('auth.forgot-password');
    }
 
-   public function sendResetLink(){
+   public function sendResetLink(ForgotPasswordRequest $request){
 
+   $status = $this->passwordResetService->sendResetLink($request->validated());
+
+   return $status === Password::ResetLinkSent ?
+    back()->with(['status'=>__($status)]) :
+    back()->withErrors(['email' =>__($status)]);
+   }
+
+   public function showResetform(string $token){
+    return view('auth.reset-password',['token'=> $token]);
+   }
+
+   public function resetPassword(ResetPasswordRequest $request){
+    $status= $this->passwordResetService->resetPassword(
+        $request->only('email', 'password', 'password_confirmation','token')
+    );
+    return $status === Password::PasswordReset ?
+    redirect()->route('login')->with('status',__($status)) :
+    back()->withErrors(['email' =>[__($status)]]);
    }
 }
--- app/Services/PasswordReset/PasswordResetService.php
+++ app/Services/PasswordReset/PasswordResetService.php
@@ -2,13 +2,39 @@
 
 namespace App\Services\PasswordReset;
 
+use Illuminate\Support\Str;
+use App\Models\User;
+use Illuminate\Auth\Events\PasswordReset;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Password;
+
+
 class PasswordResetService
 {
-    /**
-     * Create a new class instance.
-     */
-    public function __construct()
-    {
-       
-    }
+//    @param array{email: string} $data
+
+   public function sendResetLink(array $data) : string{
+   return Password::sendResetLink([
+    'email' =>  $data['email'],
+   ]);
+   }
+
+   public function resetPassword(array $credentials) {
+    return Password::reset(
+        $credentials, function(User $user, string $password){
+            $this->updatePassword($user, $password);
+        }
+    );
+   }
+
+   //update user password and invalidate remember me session
+
+   protected function updatePassword(User $user, string $password){
+    $user->forceFill([
+        'password' => Hash::make($password),
+    ])->setRememberToken(Str::random(60));
+
+    $user->save();
+    event(new passwordReset($user));
+   }
 }
